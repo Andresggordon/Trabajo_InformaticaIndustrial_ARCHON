87906b78d8b196eeed3dcf110838d8c0331b4869
estheerbj <esther.bracamonte@alumnos.upm.es>
2026-03-14 18:53:12 +0100

    Código menú con solo fondo puesto correcto

--- src/Menu.h
+++ src/Menu.h
@@ -2,30 +2,26 @@
 #include "ETSIDI.h"
 
 enum class Modos_juego {
-	MENU, Un_jugador, Dos_jugadores, Ranking
+    MENU, Un_jugador, Dos_jugadores, Ranking
 };
 
 class Menu {
 public:
-    Menu(); // Encargado de cargar las iamgenes
-    void dibuja();  // dibuja el menú                   
-    void update(int x, int y);   // Posición ratón    
-    Modos_juego click(int x, int y);  // devulve lo que ha cliqueado
+    Menu();
+ 
+    void dibuja();
+    void update(int x, int y);
+    Modos_juego click(int x, int y);
 
 private:
     ETSIDI::Sprite* fondo;
-
-    // Logo y decoración
-    ETSIDI::Sprite* logo;
-    ETSIDI::Sprite* cabecera;
-    ETSIDI::Sprite* cartel_menu;
-
-    // Botones
-    ETSIDI::Sprite* boton_UnJugador;
-    ETSIDI::Sprite* boton_DosJugadores;
-    ETSIDI::Sprite* boton_Ranking;
-
-    ETSIDI::Sprite* indicador;
+    //ETSIDI::Sprite* logo;
+    //ETSIDI::Sprite* cabecera;
+    //ETSIDI::Sprite* cartel_menu;
+    //ETSIDI::Sprite* boton_UnJugador;
+    //ETSIDI::Sprite* boton_DosJugadores;
+   // ETSIDI::Sprite* boton_Ranking;
+   // ETSIDI::Sprite* indicador;
 
     int boton_activo;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,58 @@
+#include <Windows.h>
 #include <GL/freeglut.h>
 #include "Menu.h"
 
-Menu* menu = nullptr;
-Modos_juego estado = Modos_juego::MENU;
+Menu* menu = nullptr; //puntero al menú vacio
+Modos_juego estado = Modos_juego::MENU; //Estado actual del juego empieza en MENU
 
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-
+    glClear(GL_COLOR_BUFFER_BIT); //Borra la pantalla
     if (estado == Modos_juego::MENU) {
-        menu->dibuja();
+        menu->dibuja(); //dibuja
     }
-
-    glutSwapBuffers();
+    glutSwapBuffers(); //muestra dibujo
 }
 
 void mouseMove(int x, int y) {
     if (estado == Modos_juego::MENU) {
-        menu->update(x, y);
+        menu->update(x, y); //le dice al menú dónde está el ratón
     }
     glutPostRedisplay();
 }
 
 void mouseClick(int button, int estadoBtn, int x, int y) {
-    if (button == GLUT_LEFT_BUTTON && estadoBtn == GLUT_DOWN) {
+    if (button == GLUT_LEFT_BUTTON && estadoBtn == GLUT_DOWN) { //click izquierdo ok
         if (estado == Modos_juego::MENU) {
-            estado = menu->click(x, y);
+            estado = menu->click(x, y); // clickeas y cambia el estado
         }
     }
     glutPostRedisplay();
 }
 
-void idle() {
+void reposo() {
     glutPostRedisplay();
 }
 
 int main(int argc, char** argv) {
+    SetProcessDPIAware(); //para que no se vea borroso
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize(400, 400); // Ventana de 400x400
+    glutInitWindowSize(1000, 1000);
     glutCreateWindow("ARCHON");
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    // El (0,0) arriba a la izquierda. X crece a la derecha, Y crece hacia abajo.
-    gluOrtho2D(0, 400, 400, 0);
+    gluOrtho2D(-400, 400, -400, 400);
     glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 
     menu = new Menu();
 
     glutDisplayFunc(display);
     glutPassiveMotionFunc(mouseMove);
     glutMouseFunc(mouseClick);
-    glutIdleFunc(idle);
+    glutIdleFunc(reposo);
 
     glutMainLoop();
-
-    delete menu;
     return 0;
 }
