53a3d65893be6db50e6c27b6381d236b026ec7d1
Andresggordon <andres.ggordon@alumnos.upm.es>
2026-05-08 01:38:21 +0200

    Ajuste nuevo sprites personajes

--- src/tipo_personaje.h
+++ src/tipo_personaje.h
@@ -65,7 +65,7 @@ public:
 
     int getRadioMovimiento() const override { return 1; }
     std::string getNombreSprite() const override { return "PLC"; }
-    float getTamanoSprite() const override { return 65.0f; }
+    float getTamanoSprite() const override { return 60.0f; }
 
 };
 
@@ -129,7 +129,7 @@ public:
 
     int getRadioMovimiento() const override { return 2; }
     std::string getNombreSprite() const override { return "CINTEGRADOM"; }
-    float getTamanoSprite() const override { return 55.0f; }
+    float getTamanoSprite() const override { return 50.0f; }
 
 
 };
@@ -142,13 +142,12 @@ public:
         : Volador("MH", 35,
             Turno::TURNO_DE_TARDE,
             Movimiento::AIRE,
-            stats("Tizas", 8, 1, 6), 
-            casillaInicial) {
-    }
+            stats("Tizas", 8, 1, 6),
+            casillaInicial) {}
     Menu_habilidades menu;
     int getRadioMovimiento() const override { return 4; }
     std::string getNombreSprite() const override { return "MH"; }
-    float getTamanoSprite() const override { return 50.0f; }
+    float getTamanoSprite() const override { return 60.0f; }
 
 
 };
@@ -198,7 +197,7 @@ public:
 
     int getRadioMovimiento() const override { return 1; }
     std::string getNombreSprite() const override { return "BRAZOROBOT"; }
-    float getTamanoSprite() const override { return 65.0f; }
+    float getTamanoSprite() const override { return 60.0f; }
 
 
 };
@@ -266,7 +265,7 @@ public:
 
     int getRadioMovimiento() const override { return 2; }
     std::string getNombreSprite() const override { return "CINTEGRADOT"; }
-    float getTamanoSprite() const override { return 55.0f; }
+    float getTamanoSprite() const override { return 50.0f; }
 
 
 };
